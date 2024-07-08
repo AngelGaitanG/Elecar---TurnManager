f713ce9285e73da7342106a5bba9874756dfe6b9
Angel Gabriel Gaitan <ft49pfgrupo1@gmail.com>
2024-07-08 19:18:34 -0300

    last try

--- front/Concesionaria-turnos-HW3/src/components/navbar/navBarComponents/Logo.jsx
+++ front/Concesionaria-turnos-HW3/src/components/navbar/navBarComponents/Logo.jsx
@@ -3,7 +3,7 @@ import styles from "../Navbar.module.css"
 const Logo = () => {
     return (
         <div className={styles.logo}>
-            <img className={styles.logoImg} src="src/assets2/logo.png"/>
+            <img className={styles.logoImg} src="https://png.pngtree.com/png-clipart/20230913/original/pngtree-car-dealership-vector-png-image_11060863.png"/>
         </div>
     )
 }
