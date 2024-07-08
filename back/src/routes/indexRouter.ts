8648024b9cc2cec7b962ab7df4c2d0126efa825f
Angel Gabriel Gaitan <ft49pfgrupo1@gmail.com>
2024-07-08 19:50:30 -0300

    xd

--- back/src/routes/indexRouter.ts
+++ back/src/routes/indexRouter.ts
@@ -6,5 +6,5 @@ const router: Router = Router();
 
 router.use("/users", userRouter);
 router.use("/appointments", appointmentRouter);
-router.get("welcome", (req, res) => res.send("Welcome to my app!"))
+router.get("/welcome", (req, res) => res.send("Welcome to my app!"))
 export default router
